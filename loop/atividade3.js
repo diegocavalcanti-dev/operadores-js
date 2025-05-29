0f610f534512efd86529087a6d44411daf86a1f6
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2025-05-28 22:13:01 -0300

    loops | for, while, do while

---
+++ loop/atividade3.js
@@ -0,0 +1,15 @@
+const leia = require('readline-sync');
+
+let numero;
+let soma = 0;
+
+do {
+    numero = leia.questionInt("Digite um numero: ");
+
+    if (numero > 0) {
+        soma += numero;
+    }
+
+} while (numero !== 0);
+
+console.log(`\nA soma dos números positivos é: ${soma}`);
